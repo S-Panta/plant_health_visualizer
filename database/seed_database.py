0e8ecc4e1f4a73a2973e741172bd7d9c7dd3476b
Sabin Panta <64484313+S-Panta@users.noreply.github.com>
2026-04-14 11:51:49 -0600

    Update site information (#16)
    * cleaning code
    
    * updated value for database

--- database/seed_database.py
+++ database/seed_database.py
@@ -11,26 +11,27 @@ sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 from database.connection import DatabaseManager
 
 seeder = DatabaseManager()
+# Site code : Code used by organization that collects the data to identify the site 
+# other value can be NULL
 site_data = [
     {
         "SiteID": 1,
-        "SiteCode": "S001",
-        "SiteName": "Logan River",
-        "Latitude": 41.735,
-        "Longitude": -111.834,
+        "SiteCode": "PelicanLake",
+        "SiteName": "Pelican Lake Farms",
+        "Latitude": 40.192,
+        "Longitude": -109.682,
         "State": "UT",
     }
 ]
 
 seeder.insert_many("sites", site_data)
-
-# enter the correct value of these items and make PR
+# ndvi is dimensionless, therefore variable units id: 137
 variable_data = [
     {
         "VariableCode": "NDVI",
         "VariableName": "NDVI",
         "Speciation": "Not Applicable",
-        "VariableUnitsID": 1,
+        "VariableUnitsID": 137,
         "SampleMedium": "Land",
         "ValueType": "Field Observation",
         "IsRegular": 0,
@@ -45,27 +46,22 @@ variable_data = [
 seeder.insert_many("variables", variable_data)
 
 
-# # get_table_info("sources")
-# # enter the correct value of these items and make PR
-# source_data = [
-#     {
-#         "Organization": "US Geological Survey",
-#         "SourceDescription": "Water quality monitoring data",
-#         "SourceLink": "https://waterdata.usgs.gov",
-#         "ContactName": "test test",
-#         "Phone": "123123",
-#         "Email": "testemail@usgs.gov",
-#         "Address": "test address",
-#         "City": "Logan",
-#         "State": "UT",
-#         "ZipCode": "84321",
-#         "Citation": "USGS Water Data Repository",
-#         "MetadataID": 1
-#     }
-# ]
-# source_seeder = DatabaseSeeder()
-# source_seeder.insert_many("sources", source_data)
+# get_table_info("sources")
+# enter the correct value of these items and make PR
+source_data = [
+    {
+        "Organization": "Utah State University",
+        "SourceDescription": "NDVI measuring site",
+        "ContactName": "Utah State University",
+        "City": "Logan",
+        "State": "UT",
+        "Citation":"Utah State University",
+        "MetadataID": 0
+    }
+]
+seeder.insert_many("sources", source_data)
 
 # # This could be zero
 # get_table_info("methods")
 seeder.commit_to_database()
+print("all necessary metadata are injected to database")
